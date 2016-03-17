0d72b268e5f284e2262e1e5b0310c337aaa83bf1
leeseungje <leeseungje@naver.com>
2016-03-17 13:45:24 +0900

    Sass Study

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require("gulp"),
+	sass = require("gulp-sass"),
+	watch = require('gulp-watch');
+
+// default
+gulp.task("default", function() {
+    console.log("Hello world")
+})
+
+// compile-sass
+gulp.task('compile-sass', function () {
+    return gulp.src("./*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./css"));
+});
+
+// watch
+gulp.watch("./*.scss", ['compile-sass']);
